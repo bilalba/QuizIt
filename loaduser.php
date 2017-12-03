32c8266b1fded202a32d51bc54e064c936bdf81f
fhdsouza <froilahency@gmail.com>
2017-12-03 16:22:46 -0700

    removed db prefix

--- loaduser.php
+++ loaduser.php
@@ -10,7 +10,7 @@ if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-$sql= "CREATE TABLE `quiz`.`user_profile` ( `completed_q_array` VARCHAR(500) NULL , `solved_count_array` VARCHAR(200) NULL , `preferred_categories` VARCHAR(200) NULL , `TS_last_ques` VARCHAR(5000) NULL , `Name` VARCHAR(500) NOT NULL , `Username` VARCHAR(40) NOT NULL , `Password` VARCHAR(40) NOT NULL , `incorrectly_count_array` VARCHAR(200) NULL , `incompleted_q_array` VARCHAR(500) NULL, `module_level` VARCHAR(200) , PRIMARY KEY (`Username`)) ENGINE = InnoDB;";
+$sql= "CREATE TABLE `user_profile` ( `completed_q_array` VARCHAR(500) NULL , `solved_count_array` VARCHAR(200) NULL , `preferred_categories` VARCHAR(200) NULL , `TS_last_ques` VARCHAR(5000) NULL , `Name` VARCHAR(500) NOT NULL , `Username` VARCHAR(40) NOT NULL , `Password` VARCHAR(40) NOT NULL , `incorrectly_count_array` VARCHAR(200) NULL , `incompleted_q_array` VARCHAR(500) NULL, `module_level` VARCHAR(200) , PRIMARY KEY (`Username`)) ENGINE = InnoDB;";
 
  if(mysqli_query($link, $sql)){
      echo "Table created successfully.";
